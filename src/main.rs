1d8540a7a2d1e87c84f3ef74984d921e9029f9e7
Pan Shaun <Shaun.Pan@fil.com>
2023-10-11 22:19:55 +0800

    Another version

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ struct Repository {
 
 struct Repositories {
     search_str: String,
-    repositories: <Vec<Repository> as IntoIterator>::IntoIter,
+    repositories: Vec<Repository>,
     client: reqwest::Client,
     page: u32,
     per_page: u32,
@@ -27,41 +27,36 @@ struct Repositories {
 }
 
 impl Repositories {
-    async fn new() -> Result<Self> {
-        let search_str = env::var("SEARCH_STR").unwrap();
-        let gh_token = env::var("GH_TOKEN").unwrap();
+    async fn new(search_str: String, token: String) -> Result<Self>{
 
         Ok(Repositories {
-            search_str: search_str.to_owned(),
-            repositories: vec![].into_iter(),
+            search_str,
+            repositories: Vec::new(),
             client: reqwest::Client::new(),
             page: 0,
             per_page: 30,
             total: 0,
-            token: gh_token,
+            token,
         })
     }
 
-    async fn try_next(&mut self) -> Result<Option<Repository>> {
-        if let Some(repo) = self.repositories.next() {
-            return Ok(Some(repo));
+    async fn fetch_next_page(&mut self) -> Result<bool> {
+        if self.page>0 &&self.page * self.per_page >=self.total{
+            return Ok(false);
         }
 
-        if self.page > 0 && self.page * self.per_page >= self.total {
-            return Ok(None);
-        }
+        self.page+=1;
 
-        self.page += 1;
         let url = format!(
             "https://api.github.com/search/repositories?q={}&page={}&per_page={}",
             self.search_str, self.page, self.per_page
         );
 
-        let token = format!("bearer {}", self.token,);
+        let token = format!("bearer {}", self.token);
 
         let response = self
             .client
-            .get(url)
+            .get(&url)
             .header(ACCEPT, "application/vnd.github+json")
             .header(AUTHORIZATION, &token)
             .header("x-github-api-version", "2022-11-28")
@@ -69,41 +64,35 @@ impl Repositories {
             .send()
             .await?
             .json::<ApiResponse>()
-        .await?;
-        self.repositories = response.items.into_iter();
+            .await?;
+
+        self.repositories.extend(response.items);
         self.total = response.total_count;
-        Ok(self.repositories.next())
+
+        Ok(true)
     }
 }
 
-// impl Iterator for Repositories {
-//     type Item = Result<Repository>;
-//
-//     fn next(&mut self) -> Option<Self::Item> {
-//         match self.try_next() {
-//             Ok(Some(repo)) => Some(Ok(repo)),
-//             Ok(None) => None,
-//             Err(err) => Some(Err(err)),
-//         }
-//     }
-// }
-
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let mut repo_full_names = Vec::new();
+    let search_str = env::var("SEARCH_STR").unwrap();
+    let gh_token = env::var("GH_TOKEN").unwrap();
     let file_path = env::var("FILE_PATH").unwrap();
-    let mut repos = Repositories::new().await?;
 
-    while let Some(repo) = repos.try_next().await? {
-        
-        // println!("reverse dependency: {}", repo?.full_name);
-        repo_full_names.push(repo.full_name);
+    let mut repos = Repositories::new(search_str, gh_token).await?;
+
+    while let Ok(has_more_repos) = repos.fetch_next_page().await {
+       if !has_more_repos{
+            break;
+        }
     }
-    // for repo in Repositories::new().await? {
-    //     // println!("reverse dependency: {}", repo?.full_name);
-    //     repo_full_names.push(repo?.full_name);
-    // }
+
+    for repo in &repos.repositories{
+        repo_full_names.push(repo.full_name.clone());
+    }
 
     std::fs::write(file_path, repo_full_names.join("\n")).unwrap();
     Ok(())
 }
+
